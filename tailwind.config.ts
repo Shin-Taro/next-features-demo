cdf51579be4127d4794062a323979d71379dd1fb
Shin-Taro <sh.takahashi.113@gmail.com>
2024-01-16 23:06:36 +0900

    feat: them adjust font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
     },
     // フォントサイズ定義( https://www.figma.com/file/8JNl8iF4w9cDaanBaXauAD/Blog?node-id=21-48&t=hP9Kfvck2BC3y6gR-0 )
     fontSize: {
-      first: "3.2rem",
-      second: "2.4rem",
+      first: "3.8rem",
+      second: "2.8rem",
       third: "2rem",
       fourth: "1.6rem",
       fifth: "1.4rem",
