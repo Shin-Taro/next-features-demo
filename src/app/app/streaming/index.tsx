916007b967034e2c6e872b3abbf180a72e0d0ccc
Shin-Taro <sh.takahashi.113@gmail.com>
2024-01-18 00:01:23 +0900

    feat: add temporary streaming page

---
+++ src/app/app/streaming/index.tsx
@@ -0,0 +1,7 @@
+// This page is dynamic data fetching and streaming component on App Router page
+
+import { FC } from "react"
+
+const Streaming: FC = () => <div>Streaming</div>
+
+export default Streaming
