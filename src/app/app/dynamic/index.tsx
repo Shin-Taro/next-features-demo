792be2d6fabcfd32d112af8462a12f3ba6b9c282
Shin-Taro <sh.takahashi.113@gmail.com>
2024-01-24 19:24:16 +0900

    refactor: replace page name dynamic with on-demand

--- src/app/app/dynamic/index.tsx
+++
@@ -1,7 +0,0 @@
-// This page is dynamic data fetch on App Router page
-
-import { FC } from "react"
-
-const Dynamic: FC = () => <div>dynamic</div>
-
-export default Dynamic
---
+++ src/app/app/on-demand/index.tsx
@@ -0,0 +1,7 @@
+// This page is on-demand data fetch on App Router page
+
+import { FC } from "react"
+
+const OnDemand: FC = () => <div>on-demand</div>
+
+export default OnDemand
